aa6a26d482e734f673b374b71d2e589cd9c0f348
SerjMogilat <ABCser@ya.ru>
2022-11-20 13:24:18 +0300

    Задание с 3 по счету цифрой в числе

--- Seminar/First/5task/Program.cs
+++ Seminar/First/5task/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int digitsCount = (int)Math.Log10(num + 1); // количество разрядов
-Console.WriteLine(digitsCount); 
-int multiplier = (int)Math.Pow(10, digitsCount - 3); // множитель предпоследнего разряда
+int razr = (int)Math.Log10(num) + 1; // количество разрядов
+Console.WriteLine(razr); 
+int multiplier = (int)Math.Pow(10, razr - 3); // множитель 3 слева разряда
 Console.WriteLine(multiplier); 
-int result = num % multiplier + num / multiplier / 10 * multiplier; // все что справа от разряда + все что слева, делённое на 10
-Console.WriteLine(result); 
+int result = num % (10 * multiplier) / multiplier; 
+Console.WriteLine(result);
