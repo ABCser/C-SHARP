df57c8b7ae0ae5835e1320fcc4fae9d1b1de1cab
SerjMogilat <ABCser@ya.ru>
2022-11-20 11:14:47 +0300

    Материалы семинара1

---
+++ 1task/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 1 число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int squarenum2 = num2 * num2;
+
+if (num1 == squarenum2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
