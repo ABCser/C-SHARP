eedf6d67c73e78bcab539447fc4f96b39ffe7a74
SerjMogilat <ABCser@ya.ru>
2022-11-20 12:49:58 +0300

    раскидал по папкам

---
+++ Seminar/First/5task/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int digitsCount = (int)Math.Log10(num + 1); // количество разрядов
+Console.WriteLine(digitsCount); 
+int multiplier = (int)Math.Pow(10, digitsCount - 3); // множитель предпоследнего разряда
+Console.WriteLine(multiplier); 
+int result = num % multiplier + num / multiplier / 10 * multiplier; // все что справа от разряда + все что слева, делённое на 10
+Console.WriteLine(result); 
