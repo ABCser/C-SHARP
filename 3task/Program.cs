4befc2c92c8db2950ee4e38dc471684a7da99ccb
SerjMogilat <ABCser@ya.ru>
2022-11-20 11:45:25 +0300

    Материалы семинара1

---
+++ 3task/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = num1 * -1;
+
+while (num2 <= num1) // вывод всех чисел от num2 до num1
+{
+    Console.Write($"{num2} ");
+    num2++;
+}
+Console.WriteLine();
---
+++ 4task/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int div1 = num % 10; 
+
+Console.WriteLine(div1);
