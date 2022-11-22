44873b5400caea7fe08226f49ea3b32612373ad3
SerjMogilat <ABCser@ya.ru>
2022-11-22 21:10:21 +0300

    example10

---
+++ Lections/Lect2/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array ={ 114, 22, 321, 444, 91, 54, 16, 37, 18, 91 };
+
+int n = array.Length;
+int find = 91;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
