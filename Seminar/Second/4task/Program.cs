34382f6179a11089d7fdc2a1bca01d17169dc6dd
SerjMogilat <ABCser@ya.ru>
2022-11-26 12:03:04 +0300

    2 Семинар 4 5 и 6 задачи

---
+++ Seminar/Second/4task/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 7 == 0 && num1 % 23 == 0) Console.WriteLine($"КРАСАВА! {num1} кратно 7 и 23 одновременно! ");
+
+else Console.WriteLine($"{num1} не кратно 7 и 23 одновременно!");
---
+++ Seminar/Second/5task/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 - (num1 * num1) == 0) Console.Write($"{num2} является квадратом {num1}");
+else if (num1 - (num2 * num2) == 0)Console.Write($"{num1} является квадратом {num2}");
+else Console.Write("Ни одно из чисел не является квадратом другого");
