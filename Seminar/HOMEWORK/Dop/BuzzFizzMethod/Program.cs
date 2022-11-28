a4d8e9ffbe527e2d89f1effccb27a299490e11de
SerjMogilat <ABCser@ya.ru>
2022-11-28 18:10:58 +0300

    HW доп задачи

---
+++ Seminar/HOMEWORK/Dop/BuzzFizzMethod/Program.cs
@@ -0,0 +1,7 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 15 == 0) Console.Write("FizzBuzz "); // Кратно ли 15
+    if (i % 3 == 0) Console.Write("Fizz "); // Кратно ли 3
+    if (i % 5 == 0) Console.Write("Buzz "); // Кратно ли 5
+    else Console.Write($"{i} ");
+}
