be6d1c5cbac7ed012ab9802108f097172e5681ca
SerjMogilat <ABCser@ya.ru>
2022-11-29 18:10:17 +0300

    HW 19 Hard задача

--- Seminar/HOMEWORK/Z21/Program.cs
+++ Seminar/HOMEWORK/Z21/Program.cs
@@ -10,22 +10,22 @@ double Dist(double  x1, double  x2, double y1, double y2, double z1, double z2)
 
 try
 {
-    Console.Write("Введите координаты x координату точки A: ");
+    Console.Write("Введите x координату точки A: ");
     double x1 = Convert.ToDouble(Console.ReadLine());
 
-    Console.Write("Введите координаты y координату точки A: ");
+    Console.Write("Введите y координату точки A: ");
     double y1 = Convert.ToDouble(Console.ReadLine());
 
-    Console.Write("Введите координаты z координату точки A: ");
+    Console.Write("Введите z координату точки A: ");
     double z1 = Convert.ToDouble(Console.ReadLine());
 
-    Console.Write("Введите координаты x координату точки B: ");
+    Console.Write("Введите x координату точки B: ");
     double x2 = Convert.ToDouble(Console.ReadLine());
 
-    Console.Write("Введите координаты y координату точки B: ");
+    Console.Write("Введите y координату точки B: ");
     double y2 = Convert.ToDouble(Console.ReadLine());
 
-    Console.Write("Введите координаты z координату точки B: ");
+    Console.Write("Введите z координату точки B: ");
     double z2 = Convert.ToDouble(Console.ReadLine());
     
     Console.WriteLine($"Расстояние между точками = {Dist(x1, x2, y1, y2, z1, z2)}");
---
+++ Seminar/HOMEWORK/Z21_Hard/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// void Dist(int  x1, int  x2, int y1, int y2, int z1, int z2)
+// {
+//     double distance = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
+//     Console.WriteLine($"Расстояние между точками = {Math.Round(distance, 2)}");
+// }
+
+// try
+// {
+//     Console.Write("Введите координаты x координату точки A: ");
+//     int x1 = Convert.ToInt32(Console.ReadLine());
+
+//     Console.Write("Введите координаты y координату точки A: ");
+//     int y1 = Convert.ToInt32(Console.ReadLine());
+
+//     Console.Write("Введите координаты z координату точки A: ");
+//     int z1 = Convert.ToInt32(Console.ReadLine());
+
+//     Console.Write("Введите координаты x координату точки B: ");
+//     int x2 = Convert.ToInt32(Console.ReadLine());
+
+//     Console.Write("Введите координаты y координату точки B: ");
+//     int y2 = Convert.ToInt32(Console.ReadLine());
+
+//     Console.Write("Введите координаты z координату точки B: ");
+//     int z2 = Convert.ToInt32(Console.ReadLine());
+    
+//     Dist(x1, x2, y1, y2, z1, z2);
+// }
+
+// catch
+// {
+//     Console.WriteLine("Проверьте правильность ввода данных");
+// }
+
+// Console.Write("Введите координаты x координату точки A: ");
+// int[] str = Console.ReadLine().Split().Select(int.Parse).ToArray();
+// Console.Write( str);э
+
+Console.Write("Введите количество измерений: ");
+int dim = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты точки A: ");
+int[] a = Console.ReadLine().Split(' ');
+               int a1 = int.Parse(a[0]);
+               int a2 = int.Parse(a[1]);
+               int a3 = int.Parse(a[3]);
+Console.Write($"{b} и {a}");
+
+
+
+
