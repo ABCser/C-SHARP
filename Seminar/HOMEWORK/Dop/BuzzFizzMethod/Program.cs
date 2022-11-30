7ab64ffbe1cf31dbd4ebff84ee0016bf48beab89
SerjMogilat <ABCser@ya.ru>
2022-11-30 17:26:23 +0300

    FIZZBUZZ METHOD

--- Seminar/HOMEWORK/Dop/BuzzFizzMethod/Program.cs
+++ Seminar/HOMEWORK/Dop/BuzzFizzMethod/Program.cs
@@ -1,42 +1,13 @@
-﻿for (int i = 1; i <= 100; i++)
+﻿int[] PrintArray(int num)
 {
-    if (i % 15 == 0) Console.Write("FizzBuzz "); // Кратно ли 15
-    if (i % 3 == 0) Console.Write("Fizz "); // Кратно ли 3
-    if (i % 5 == 0) Console.Write("Buzz "); // Кратно ли 5
-    else Console.Write($"{i} ");
+    int[] array = new int[num];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (i % 15 == 0) Console.WriteLine("FizzBuzz ");
+        else if (i % 5 == 0) Console.WriteLine("Buzz ");
+        else if (i % 3 == 0) Console.WriteLine("Fizz ");
+        else Console.WriteLine($"{i} ");
+    }
+    return array;
 }
-
-
-// int[] PrintArray(int num)
-// {
-//     int[] array = new int[num + 1];
-//     for (int i = 1; i < array.Length; i++)
-//     {
-//         array[i] = i;
-//         Console.WriteLine($"{array[i]} ");
-
-//     }
-//     return array;
-// }
-
-
-
-// // int[] Replace(int[] n)
-// // {
-// //     int [] result = ;
-// //     for(int i=0; i<100; i++)
-// //     {
-// //         if(n[i] % 15 == 0)  result = result * 100;
-// //         if(n[i] % 5 == 0) result = result * 10;
-// //         if(n[i] % 3 == 0) result = result * 1000;
-// //         else result = n[i];
-// //     }
-
-// //     return result;
-// // }
-
-
-// int[] fb  = PrintArray(100);
-// Console.WriteLine($"{Replace(fb)} ");
-
-
+PrintArray(101);
